3ec8891c561c2cdb8eed1a4d13a0df34bc5bbfa1
sv1979 <sergeynew37@gmail.com>
2019-08-20 12:52:23 +1200

    Jquery and bootstrap added

--- src/js/app.js
+++ src/js/app.js
@@ -1,1 +1,18 @@
-console.log('test');
+window.Popper = require('popper.js');
+
+/**
+ * We'll load jQuery and the Bootstrap jQuery plugin which provides support
+ * for JavaScript based Bootstrap features such as modals and tabs. This
+ * code may be modified to fit the specific needs of your application.
+ */
+
+window.$ = window.jQuery = require('jquery');
+
+require('bootstrap');
+
+$(document).ready(function(){
+    console.log('test');
+});
+
+
+
